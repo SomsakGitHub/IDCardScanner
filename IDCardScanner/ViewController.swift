776c4c0ad2a07c0df59cc21c91f7fc58ea341cac
somsakk <somsakk@tisco.co.th>
2023-10-10 19:01:43 +0700

    First commit.

---
+++ IDCardScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IDCardScanner
+//
+//  Created by somsakk02060 on 10/10/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
